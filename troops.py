6b0df2ce77871f21c42084b5ea63cbd7abc9a958
AlexMarinkovich <alex-989@live.com>
2025-11-14 17:13:34 -0500

    fixed trait dummies being in troop pool

--- troops.py
+++ troops.py
@@ -5,14 +5,15 @@ class Troop():
     instance_count = 0
 
     def __init__(self, cost: int, trait1: Trait, trait2: Trait, *names: str):
-        Troop.all_troops.add(self)
         self.cost = cost
         self.trait1 = trait1
         self.trait2 = trait2
         self.names = names
         self.meta_ranking = Troop.instance_count
-        Troop.instance_count += 1
         self.image = f"images/troops/{self.names[0]}.webp"
+        if names[0] == "traitdummy": return
+        Troop.all_troops.add(self)
+        Troop.instance_count += 1
     
     def __repr__(self) -> str:
         return self.names[0]
@@ -25,14 +26,14 @@ Troop(5,    Trait.CLAN,       Trait.AVENGER,      "archerqueen", "aq", "queen")
 Troop(3,    Trait.CLAN,       Trait.JUGGERNAUT,   "valkyrie")
 Troop(4,    Trait.ACE,        Trait.BRAWLER,      "megaknight")
 Troop(3,    Trait.ACE,        Trait.AVENGER,      "pekka")
-Troop(3,    Trait.UNDEAD,     Trait.BRAWLER,      "giantskeleton", "gs")
+Troop(3,    Trait.UNDEAD,     Trait.BRAWLER,      "giantskeleton", "gskeleton")
 Troop(4,    Trait.UNDEAD,     Trait.AVENGER,      "witch")
 Troop(2,    Trait.NOBLE,      Trait.JUGGERNAUT,   "knight")
 Troop(5,    Trait.UNDEAD,     Trait.JUGGERNAUT,   "skeletonking", "skellyking")
 Troop(3,    Trait.GOBLIN,     Trait.RANGER,       "dartgoblin")
 Troop(2,    Trait.GOBLIN,     Trait.BLASTER,      "speargoblin")
 Troop(2,    Trait.GOBLIN,     Trait.ASSASSIN,     "goblin")
-Troop(4,    Trait.GOBLIN,     Trait.JUGGERNAUT,   "goblinmachine")
+Troop(4,    Trait.GOBLIN,     Trait.JUGGERNAUT,   "goblinmachine", "gmachine", "machine")
 Troop(3,    Trait.NOBLE,      Trait.BRAWLER,      "prince")
 Troop(3,    Trait.NOBLE,      Trait.BLASTER,      "musketeer")
 Troop(5,    Trait.NOBLE,      Trait.ASSASSIN,     "goldenknight")
