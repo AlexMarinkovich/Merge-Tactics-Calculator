800070a690b566622655092f78b07bbc3a5b4cf3
AlexMarinkovich <alex-989@live.com>
2025-11-24 23:27:30 -0500

    changed troop order

--- traits.py
+++ traits.py
@@ -3,18 +3,13 @@ from enum import IntEnum, auto
 class Trait(IntEnum):
     CLAN = 0
     ACE = auto()
-    # ELECTRIC = auto()
     UNDEAD = auto()
     GOBLIN = auto()
     NOBLE = auto()
-    # FIRE = auto()
     BRAWLER = auto()
     RANGER = auto()
     BLASTER = auto()
-    # AVENGER = auto()
     ASSASSIN = auto()
-    # JUGGERNAUT = auto()
-    # MAGE = auto()
     BRUTALIST = auto()
     GIANT = auto()
     PEKKA = auto()
